f6c9dc7e4d9e554a9662105cfd38ca9f900fa9b5
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-03-07 15:28:02 +0700

    fix chapter 3

--- section-03-working-with-tables.sql
+++ section-03-working-with-tables.sql
@@ -1,19 +1,18 @@
-# ------------------------------------------------------ ONE-TO-MANY and MANY-TO-ONE relationship
+---- ONE-TO-MANY or MANY-TO-ONE relationship
 
-# a user HAS MANY photos (one-to-many)
-# a photo HAS ONE user (many-to-one)
+--  a user HAS MANY photos (one-to-many)
+--  a photo HAS ONE user (many-to-one)
 
-# ------------------------------------------------------ ONE-TO-ONE and MANY-TO-MANY relationship
+---- ONE-TO-ONE and MANY-TO-MANY relationship
 
-# o-t-o country-capital
-# m-t-m students-classes, movies-actors
+--  o-t-o country-capital
+--  m-t-m students-classes, movies-actors
 
-# ------------------------------------------------------ PRIMARY KEYS AND FOREIGN KEYS
+---- PRIMARY KEYS AND FOREIGN KEYS
 
-# the MANY side get the FOREIGN KEY COLUMN
+--  the MANY side get the FOREIGN KEY COLUMN
 
-
-# ------------------------------------------------------ AUTO GENERATED ID
+---- AUTO GENERATED ID
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -29,7 +28,9 @@ VALUES
 
 SELECT * FROM users;
 
-# ------------------------------------------------------ CREATE FOREIGN KEY
+DROP TABLE users;
+
+---- CREATE FOREIGN KEY
 
 CREATE TABLE photos (
     id SERIAL PRIMARY KEY,
@@ -43,7 +44,7 @@ VALUES
     
 SELECT * FROM photos;
 
-# ------------------------------------------------------ RUNNING QUERIES ON ASSCOCIATED DATA
+---- RUNNING QUERIES ON ASSOCIATED DATA
 
 INSERT INTO photos(url, user_id)
 VALUES
@@ -63,68 +64,76 @@ JOIN users ON users.id = photos.user_id;
 SELECT url, username FROM photos
 JOIN users ON users.id = photos.user_id;
 
-# ------------------------------------------------------ FOREIGN KEY CONSTRAIN AROUND INSERTION (DATA CONSISTENCY)
+---- FOREIGN KEY CONSTRAINT AROUND INSERTION (DATA CONSISTENCY)
 
 SELECT * FROM users;
 SELECT * FROM photos;
 
-# yang bawah ini harusnya error, tapi di goormide enggak
 INSERT INTO photos (url, user_id)
 VALUES ('http://a.jpg', 999);
 
-# boleh
 INSERT INTO photos (url, user_id)
 VALUES ('http://b.jpg', null);
 
-# ALTER TABLE photos ALTER COLUMN user_id DROP NOT NULL;
+ALTER TABLE photos ALTER COLUMN user_id DROP NOT NULL; -- drop not null constraint
 
-# ------------------------------------------------------ CONSTRAINTS AROUND DELETION
+---- CONSTRAINTS AROUND DELETION
 
-# ON DELETE RESTRICT (DEFAULT). INI HARUSNYA ERROR, TAPI DI GOORMIDE ENGGAK
+-- ON DELETE RESTRICT (DEFAULT).
 DELETE FROM users
 WHERE id = 3;
 
+-- ON DELETE RESTRICT => thrown an error
+-- ON DELETE CASCADE => all photos with user_id = 3 will be deleted
+-- ON DELETE SET NULL => all user_id in photos with user_id = 3 will be set to null
+-- ON DELETE SET DEFAULT => all user_id in photos with user_id = 3 will be set to default value (if there is a default value)
+-- ON DELETE NO ACTION
 
-# ------------------------------------------------------ TESTING DELETE CONSTRAINTS
+---- TESTING DELETE CONSTRAINTS
 
 CREATE TABLE photos (
-id SERIAL PRIMARY KEY,
-url VARCHAR(200),
-user_id INTEGER REFERENCES users(id) ON DELETE SET NULL;
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(200),
+    user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE photos (
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(200),
+    user_id INTEGER REFERENCES users(id) ON DELETE SET NULL
 );
 
 
 CREATE TABLE photos (
-id SERIAL PRIMARY KEY,
-url VARCHAR(200),
-user_id INTEGER REFERENCES users(id) ON DELETE CASCADE;
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(200),
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE photos (
-id SERIAL PRIMARY KEY,
-url VARCHAR(200),
-user_id INTEGER DEFAULT(2) REFERENCES users(id) on DELETE SET DEFAULT
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(200),
+    user_id INTEGER DEFAULT(2) REFERENCES users(id) on DELETE SET DEFAULT
 );
  
 INSERT INTO photos (url, user_id)
 VALUES
-('http:/one.jpg', 4),
-('http:/two.jpg', 1),
-('http:/25.jpg', 1),
-('http:/36.jpg', 1),
-('http:/754.jpg', 2),
-('http:/35.jpg', 3),
-('http:/256.jpg', 4);
+    ('http:/one.jpg', 4),
+    ('http:/two.jpg', 1),
+    ('http:/25.jpg', 1),
+    ('http:/36.jpg', 1),
+    ('http:/754.jpg', 2),
+    ('http:/35.jpg', 3),
+    ('http:/256.jpg', 4);
 
 
 DROP TABLE photos;
 
 SELECT * FROM photos;
 
-# HARUSNYA TABLE PHOTOS BERKURANG ROW NYA, YANG USER_ID = 1 ILANG, TAPI DI GOORMIDE ENGGAK.
 DELETE FROM users
 WHERE id = 1;
 
-# ------------------------------------------------------ ADDING SOME COMPLEXITY
+---- ADDING SOME COMPLEXITY
 
