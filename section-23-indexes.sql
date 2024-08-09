dacfcd09aad5dff0df6377256ea1dd0989665587
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-08-09 13:26:47 +0700

    complete section 23

--- section-23-indexes.sql
+++ section-23-indexes.sql
@@ -21,3 +21,12 @@ CREATE EXTENSION pageinspect;
 
 SELECT *
 FROM bt_metap('users_username_idx');
+
+SELECT *
+FROM bt_page_items('users_username_idx', 3);
+
+SELECT ctid, * FROM users WHERE username = 'Aaliyah.Hintz';
+
+SELECT oid, datname FROM pg_database;
+
+SHOW data_directory;
